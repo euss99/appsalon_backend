2cefec1b7ce915e820ad6c19398e5804444e6043
Uriel Solis <euss99@hotmail.com>
2023-08-31 16:10:08 -0600

    Added styles to email verify user

--- emails/authEmailService.js
+++ emails/authEmailService.js
@@ -11,16 +11,22 @@ export async function sendEmailVerification({ name, email, token }) {
 
   // Enviar el email
   const info = await transporter.sendMail({
-    from: "appSalon@correo.com", // Quien envía el email
+    from: "AppSalon <appSalon@correo.com>", // Quien envía el email
     to: email, // A quien se le envía el email
-    subject: "AppSalon - Confirma tu cuenta", // Asunto del email
-    text: "Hola, confirma tu cuenta", // Contenido del email
+    subject: "Confirma tu cuenta en AppSalon", // Asunto del email
+    text: "Hola,", // Contenido del email en texto plano
     html: `
-        <p>Hola ${name}, confirma tu cuenta en AppSalon</p>
-        <p>Tu cuenta esta casi lista, solo debes confirmarla en el siguiente enlace:</p>
-        <a href="http://localhost:4000/api/auth/verify/${token}">Confirmar cuenta</a>
-        <p>Si tu no creaste esta cuenta, puedes ignorar este mensaje</p>
-    `, // Contenido del email en HTML
+      <div style="font-family: Arial, sans-serif; padding: 20px; border: 1px solid #e0e0e0; border-radius: 5px;">
+        <h2 style="color: #333;">¡Hola ${name},</h2>
+        <p style="font-size: 16px;">Estamos emocionados de darte la bienvenida a AppSalon!</p>
+        <p style="font-size: 16px;">Tu cuenta está casi lista. Solo debes confirmarla haciendo clic en el siguiente enlace:</p>
+        <p style="font-size: 16px;"><a href="http://localhost:4000/api/auth/verify/${token}" style="color: #007bff; text-decoration: underline;">Confirmar cuenta</a></p>
+        <p style="font-size: 16px;">Si no creaste esta cuenta, puedes ignorar este mensaje.</p>
+        <p style="font-size: 16px;">¡Gracias por unirte a nuestra comunidad!</p>
+        <p style="font-size: 16px;">Saludos,</p>
+        <p style="font-size: 16px;">El equipo de AppSalon</p>
+      </div>
+      `, // Contenido del email en HTML con estilos y una imagen
   });
 
   // info nos devuelve un objeto con la información del email enviado
