a873217aae7027abddf60e46fbd0e5f23d027d99
Uriel Solis <euss99@hotmail.com>
2023-08-31 16:42:20 -0600

    changed link verity to frontend url, and create new env

--- emails/authEmailService.js
+++ emails/authEmailService.js
@@ -3,10 +3,10 @@ import { createTransporter } from "../config/nodemailer.js";
 export async function sendEmailVerification({ name, email, token }) {
   // Nos crea el transporter que tendrá los métodos para enviar emails, en este caso usamos Mailtrap
   const transporter = createTransporter(
-    "sandbox.smtp.mailtrap.io",
-    2525,
-    "e59b1a67ee3afb",
-    "340abe8b832a27"
+    process.env.EMAIL_HOST,
+    process.env.EMAIL_PORT,
+    process.env.EMAIL_USER,
+    process.env.EMAIL_PASS
   );
 
   // Enviar el email
@@ -20,7 +20,7 @@ export async function sendEmailVerification({ name, email, token }) {
         <h2 style="color: #333;">¡Hola ${name},</h2>
         <p style="font-size: 16px;">Estamos emocionados de darte la bienvenida a AppSalon!</p>
         <p style="font-size: 16px;">Tu cuenta está casi lista. Solo debes confirmarla haciendo clic en el siguiente enlace:</p>
-        <p style="font-size: 16px;"><a href="http://localhost:4000/api/auth/verify/${token}" style="color: #007bff; text-decoration: underline;">Confirmar cuenta</a></p>
+        <p style="font-size: 16px;"><a href="${process.env.FRONTEND_URL}/auth/confirmar-cuenta/${token}" style="color: #007bff; text-decoration: underline;">Confirmar cuenta</a></p>
         <p style="font-size: 16px;">Si no creaste esta cuenta, puedes ignorar este mensaje.</p>
         <p style="font-size: 16px;">¡Gracias por unirte a nuestra comunidad!</p>
         <p style="font-size: 16px;">Saludos,</p>
